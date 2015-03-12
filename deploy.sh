8edf2d92f37b1c43ef54217a53bf81325973f9a3
Sean Whitton <spwhitton@spwhitton.name>
2015-03-12 08:50:34 +0000

    deploy script deploys assets

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 # simple deploy script for demeter
 
-cp -RL dist/build/sariulclocks.cgi/sariulclocks.cgi $HOME/html/
+cp -RL assets dist/build/sariulclocks.cgi/sariulclocks.cgi $HOME/html/
 # TODO: run strip on binary
