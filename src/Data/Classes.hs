ddc970d33e39f1a1c62ff2773018530a799aa77e
Sean Whitton <spwhitton@spwhitton.name>
2015-03-13 03:53:22 +0000

    Sariul 2015 classes and initial scores setup

--- src/Data/Classes.hs
+++ src/Data/Classes.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE TupleSections #-}
+
 module Data.Classes (zeroScores) where
 
 import Types.Classes
+import Types.Scores
 
 sariulGrade5 = GradeFive 1.5
 sariulGrade6 = GradeSix  1
@@ -8,12 +11,12 @@ sariulGrade6 = GradeSix  1
 sariulClasses :: [Class]
 sariulClasses =
     [ Class sariulGrade5     1    25
-    , Class sariulGrade5     2    25
+    , Class sariulGrade5     2    24
     , Class sariulGrade5     3    25
-    , Class sariulGrade6     1    25
-    , Class sariulGrade6     2    25
-    , Class sariulGrade6     3    25
-    , Class sariulGrade6     4    25 ]
+    , Class sariulGrade6     1    23
+    , Class sariulGrade6     2    23
+    , Class sariulGrade6     3    23
+    , Class sariulGrade6     4    21 ]
 
 zeroScores :: ScoresList
-zeroScores = undefined
+zeroScores = foldr ((:) . (,emptyScore)) [] sariulClasses
--- src/Types/Scores.hs
+++ src/Types/Scores.hs
@@ -3,3 +3,5 @@ module Types.Scores where
           --       points   seconds wasted this week
 data Score = Score Int      Int
            deriving (Show, Eq, Ord)
+
+emptyScore = Score 0 0
