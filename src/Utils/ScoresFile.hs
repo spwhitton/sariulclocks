2c21e89f13d72908e6e2ef930799707a2961fdd6
Sean Whitton <spwhitton@spwhitton.name>
2015-03-13 01:09:28 +0000

    code to read scores file

--- src/Utils/ScoresFile.hs
+++ src/Utils/ScoresFile.hs
@@ -5,7 +5,11 @@ import Types.Scores
 import Types.Classes
 import Data.Time.Clock.POSIX (getPOSIXTime)
 import Control.Applicative ((<$>))
-import System.IO (withFile)
+import System.IO (readFile, writeFile)
+import Data.List (sort)
+import System.Directory (getDirectoryContents, getCurrentDirectory)
+import System.FilePath (takeExtension)
+import Control.Monad (liftM)
 
 scoresToCSV :: ScoresList -> String
 scoresToCSV = undefined
@@ -15,7 +19,12 @@ scoresFromCSV = undefined
 
 -- read to scores-XX.csv where XX is largest timestamp
 readScoresFile :: IO ScoresList
-readScoresFile = undefined
+readScoresFile = do
+    curDir <- getCurrentDirectory
+    -- what if there's no CSV file yet?  head will throw an exception.  handle it.
+    filename <- liftM (head . reverse . sort . filter csvs) $ getDirectoryContents curDir
+    scoresFromCSV <$> readFile filename
+  where csvs path = takeExtension path == ".csv"
 
 -- writes to score-XX.csv where XX is unix timestamp: a simple-minded logging
 writeScoresFile :: ScoresList -> IO ()
