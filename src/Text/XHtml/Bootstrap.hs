60b0b9278533f746369be2d5ebee85e80b55052e
Sean Whitton <spwhitton@spwhitton.name>
2015-03-17 06:14:44 +0000

    add (#=) ability to Bootstrap.hs

--- src/Text/XHtml/Bootstrap.hs
+++ src/Text/XHtml/Bootstrap.hs
@@ -2,12 +2,12 @@ module Text.XHtml.Bootstrap where
 
 import Text.XHtml
 
--- # ought to be for Id, something else for class ...
-
 (#)          :: ADDATTRS a => a -> String -> a
 e # theClass = e ! [strAttr "class" theClass]
 
+(#=)          :: ADDATTRS a => a -> String -> a
+e #= theClass = e ! [strAttr "id" theClass]
+
 bsButton                   :: HTML a => String -> String -> a -> Html
-bsButton id theClass label = anchor ! [ strAttr "id" id
-                                    , strAttr "class" theClass]
+bsButton id theClass label = anchor # theClass #= id
                              << label
