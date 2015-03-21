d789bb983b95941b5854b5980eded3208feb983a
Sean Whitton <spwhitton@spwhitton.name>
2015-03-21 18:08:31 +0900

    using boxes library instead

--- src/Text/Table.hs
+++
@@ -1,9 +0,0 @@
-module Text.Table ( tableFromRows
-                  , TableHeader(..)
-                  , TableRows(..)) where
-
-data Show a => TableHeader a = TableHeader [a]
-data Show a => TableRows a   = TableRows [[a]]
-
-tableFromRows :: TableHeader a -> TableRows a -> String
-tableFromRows = undefined
