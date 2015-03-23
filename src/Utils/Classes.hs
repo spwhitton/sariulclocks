72f2d4cb2a296887c74bb49b364f8df3adf43bf7
Sean Whitton <spwhitton@spwhitton.name>
2015-03-23 06:09:42 +0000

    rm old stuff

--- src/Utils/Classes.hs
+++ src/Utils/Classes.hs
@@ -11,17 +11,6 @@ import Data.Function (on)
 import Text.XHtml.Bootstrap
 import Control.Monad (liftM, liftM2)
 
--- Make the columns with rankingColumns, and then transpose them so
--- that we can make HTML.
-
--- TODO: Do this as two composed folds (point-free), rather than
--- three: first one accumulates to list of three rows (each step adds
--- to all three rows) and second turns list of three rows into Html.
---
--- This also means that we can have the whole column with a meaningful
--- blue background colour for the current class and the winning two
--- classes, not only the first row.
-
 rankings'                    :: Maybe Class -> ScoresList -> ([Html], [Html], [Html])
 rankings' theClass theScores = foldr step ([], [], []) . sortBy (flip $ (compare `on` snd)) $ theScores
   where
@@ -43,16 +32,6 @@ rankings = do
     return (thediv # "container" << thediv # "col-md-12" <<
             table # "table table-bordered table-centered table-chunky"
             << ((tr << top) +++ (tr << middle) +++ (tr << bottom)))
-
--- rankings                     :: Maybe Class -> ScoresList -> Html
--- rankings currentClass scores = table ! [htmlAttr "class" (noHtml +++ "table table-bordered table-centered table-chunky")]
---                                << foldr (\row acc -> (tr << row) +++ acc) noHtml (rankings' currentClass scores)
-
--- rankings'                     :: Maybe Class -> ScoresList -> [Html]
--- rankings' currentClass scores = foldr step [noHtml, noHtml, noHtml] (rankingColumns currentClass scores)
---   where
---     step (first, second, third) [firstRow, secondRow, thirdRow] =
---         [first +++ firstRow, second +++ secondRow, third +++ thirdRow]
 
 secondsToTime         :: Int -> String
 secondsToTime n       = minutes' ++ ":" ++ seconds'
